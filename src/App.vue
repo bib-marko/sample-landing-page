27f9f55bc807a0fd29324c3f0bac619bdb8f6615
MarkO <mark_anthony@megabet.com>
2025-09-16 15:03:12 +0800

    fix: deployment

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <!-- <SecureText /> -->
-  <!-- <LandingPage /> -->
   <Landing />
 </template>
 
 <script setup lang="ts">
-// import SecureText from "./components/SecureText.vue";
-// import LandingPage from "./components/LandingPage.vue";
 import Landing from "./components/Landing.vue";
 </script>
